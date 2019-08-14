ebb277127efed1c4f38ec722361b48606783b1ab
darwinum <dun34@hotmail.com>
2019-08-14 16:09:54 -0500

    inicio servicio

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import 'rxjs/Rx';
 
 
 @Injectable()
 export class YoutubeService {
-  youtubeUrl = 'https://www.googleapis.com/youtube/v3/playlistItems';
+  youtubeUrl = 'https://www.googleapis.com/youtube/v3';
   apiKey = 'AIzaSyDx-xX0OId8BPqCLsZfcqsew4vgwoUAlhs';
   constructor(public http: HttpClient) { }
 
   getVidoes() {
+    const url = `${this.youtubeUrl}/playlistItems`;
+   /*  this.http.get(url).map(respuesta => {
 
+    })); */
   }
 }
